52a3649da5c5ec6385a640a581b391930b6a34c9
Jonas Flodén <jonas@koalasoft.se>
2012-05-22 13:47:30 +0200

    Initial commit

---
+++ KoalaRegistrationBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Koala\RegistrationBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class KoalaRegistrationBundle extends Bundle
+{
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# KoalaRegistrationBundle
+This is a Symfony bundle to handle registrations for different kinds of events
